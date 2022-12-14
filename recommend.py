7d8e5e7fbe33a08b038ac337df28941437d9a56c
joynatalie <joynatalie1906@gmail.com>
2022-12-15 01:55:37 +0300

    updates

--- recommend.py
+++ recommend.py
@@ -44,3 +44,23 @@ def main():
 
 if __name__ == '_main_':
     main()
+
+import sys
+
+if len(sys.argv) > 1:
+    artist_name = sys.argv[1]
+else:
+    artist_name = 'ariana grande'
+
+
+result = sp.search(q='artist:' + artist_name, type='artist')
+try:
+    name = result['artists']['items'][0]['name']
+    uri = result['artists']['items'][0]['uri']
+
+    related = sp.artist_related_artists(uri)
+    print('Related artists for', name)
+    for artist in related['artists']:
+        print('  ', artist['name'])
+except BaseException:
+    print("usage show_related.py [artist-name]")
